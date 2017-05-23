49cc63b86f5a59c57c28231f387686837c657a02
Lazar Telebak <lazartelebak@gmail.com>
2017-05-23 21:12:41 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -4,15 +4,19 @@ from splinter import Browser
 from selenium import webdriver
 
 
-with Browser('phantomjs') as browser:
+with Browser("phantomjs") as browser:
     browser.driver.set_window_size(1280, 1024)
 
     # Visit URL
     url = "https://www.google.rs/search?q=bitcoin&tbm=nws&cad=h"
     browser.visit(url)
+    print 'loaded url'.
     sleep(2)
+    print 'in sleep function.'
 
     titles = browser.find_by_xpath('//*[@class="l _PMs"]')
+    print titles
+
     times = browser.find_by_xpath('//*[@class="f nsa _QHs"]')
     urls = browser.find_by_xpath('//*[@class="top _xGs _SHs"]')
     for title, time, url in zip(titles, times, urls):
